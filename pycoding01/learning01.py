7399719209af4faf6fa1358d72529410a624949a
Leedokyung4444 <kt01074122362@gmail.com>
2024-08-27 15:53:06 +0900

    Add files via upload

--- pycoding01/learning01.py
+++ pycoding01/learning01.py
@@ -2,4 +2,11 @@ print('Hello World!!')
 
 F = 10
 S = 20
-print(F+S)
+print(F*S)
+
+F = int(input('첫번째 숫자를 입력:'))
+S = int(input('첫번째 숫자를 입력:'))
+print ('당신의 나이는',F+S,'입니다.')
+
+year = (int(input'생년 입력:'))
+print()
