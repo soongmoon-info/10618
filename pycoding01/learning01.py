58f0b5048b8df172c6944094f3c56016d3fbfb3e
Leedokyung4444 <kt01074122362@gmail.com>
2024-09-03 15:49:33 +0900

    Add files via upload

--- pycoding01/learning01.py
+++ pycoding01/learning01.py
@@ -8,5 +8,10 @@ F = int(input('첫번째 숫자를 입력:'))
 S = int(input('첫번째 숫자를 입력:'))
 print ('당신의 나이는',F+S,'입니다.')
 
-year = (int(input'생년 입력:'))
+year = int(input('생년 입력:'))
 print()
+
+키 = float(input('키: '))/100
+몸무게 = float(input('몸무게: '))
+print('BMI:' ,몸무게/키**2)
+
---
+++ pycoding02/learning01.py
@@ -0,0 +1,14 @@
+x=20
+if x >= 10 :
+  print('조건이 ??일 때 출력')
+print('이건 언제 출력할까??')
+
+age =int(input('니 나이 써라'))
+if age <= 13 :
+  print('넌 좆밥이다')
+else :
+  if age <= 17:
+    print('청소년')
+  else:
+    print('어른')
+  
