fa60dad7c83c70d9f8dc962ab09211e92ac1922c
Leedokyung4444 <kt01074122362@gmail.com>
2024-09-10 15:47:07 +0900

    Add files via upload

---
+++ 10618-main/pycoding02/learning01.py
@@ -0,0 +1,18 @@
+B = int(input('배터리 잔량:'))
+if B <= 0:
+  print('종료')
+elif B <= 10:
+  print('충전 ㄱㄱ')
+elif B <= 20:
+  print('저전력')
+else:
+  print('장상작동')
+
+
+  tier = '브론즈'
+  cha = 'yasuo'
+  if tier == 'bronze' and cha == 'yasuo':
+    print('scientist')
+
+
+call = input('-를 제외하고 입력해주세요.')
